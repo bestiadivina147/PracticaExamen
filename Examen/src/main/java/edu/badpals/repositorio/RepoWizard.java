865dc03f67b94c3d403e40ffe3a6bda6e921c225
bestiadivina147 <diegopc147@gmail.com>
2024-06-16 12:47:15 +0200

    fix(RepoWizard.java): change the implements

--- Examen/src/main/java/edu/badpals/repositorio/RepoWizard.java
+++ Examen/src/main/java/edu/badpals/repositorio/RepoWizard.java
@@ -1,9 +1,9 @@
 package edu.badpals.repositorio;
 import edu.badpals.domain.Wizard;
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class RepoWizard implements PanacheEntityBase<Wizard,String> {
+public class RepoWizard implements PanacheRepositoryBase<Wizard,String> {
     
 }
--- Examen/src/main/java/edu/badpals/repositorio/Repositorio.java
+++ Examen/src/main/java/edu/badpals/repositorio/Repositorio.java
@@ -4,4 +4,6 @@ import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class Repositorio {
+    @Inject
+    RepoWizard repoWizard;
 }

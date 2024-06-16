ca6680ac7f4e68c3bf9793e563633a41036f4b32
bestiadivina147 <diegopc147@gmail.com>
2024-06-16 14:48:55 +0200

    feat(ServiceItem.java): add all methods to ServiceItem.java

--- Examen/src/main/java/edu/badpals/servicice/ServiceItem.java
+++ Examen/src/main/java/edu/badpals/servicice/ServiceItem.java
@@ -1,5 +1,9 @@
 package edu.badpals.servicice;
 
+import java.util.List;
+import java.util.Optional;
+
+import edu.badpals.domain.MagicalItem;
 import edu.badpals.repositorio.Repositorio;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -8,5 +12,21 @@ import jakarta.inject.Inject;
 public class ServiceItem {
     @Inject
 	 Repositorio repo;
-     
+    
+    Optional<MagicalItem> cargaItem(String name){
+        return repo.loadItem(name);
+    }
+    Optional<MagicalItem> cargaItem(MagicalItem item){
+        return repo.loadItem(item);
+    }
+    List<MagicalItem> cargaItems(String name){
+        return repo.loadItems(name);
+    }
+    Optional<MagicalItem> creaItem(MagicalItem item){
+        repo.createItem(item.getName(), item.getQuality(), item.getType());
+        return repo.loadItem(item);
+    }
+    void eliminaItem(MagicalItem item){
+        repo.deleteItem(item);
+    }
 }

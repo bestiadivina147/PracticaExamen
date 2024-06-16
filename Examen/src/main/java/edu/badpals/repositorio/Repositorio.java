c0199f08b27718525cd1b3542bd432eb1a4edbf5
bestiadivina147 <diegopc147@gmail.com>
2024-06-16 13:01:56 +0200

    feat(Repositorio.java): add loadItem(String name)

--- Examen/src/main/java/edu/badpals/repositorio/Repositorio.java
+++ Examen/src/main/java/edu/badpals/repositorio/Repositorio.java
@@ -2,6 +2,7 @@ package edu.badpals.repositorio;
 
 import java.util.Optional;
 
+import edu.badpals.domain.MagicalItem;
 import edu.badpals.domain.Wizard;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -10,8 +11,13 @@ import jakarta.inject.Inject;
 public class Repositorio {
     @Inject
     RepoWizard repoWizard;
+    @Inject
+    RepoItem repoItem;
     public Optional<Wizard> loadWizard(String name){
        return this.repoWizard.findByIdOptional(name);
 
+    }
+    public Optional<MagicalItem> loadItem(String name){
+        return this.repoItem.find("name", name).firstResultOptional();
     }
 }

b9190b3d7a463d0ede30715e631e3fdb880dc1f7
bestiadivina147 <diegopc147@gmail.com>
2024-06-16 12:55:31 +0200

    feat(Repositorio.java): add  loadWizard

--- Examen/src/main/java/edu/badpals/repositorio/Repositorio.java
+++ Examen/src/main/java/edu/badpals/repositorio/Repositorio.java
@@ -1,9 +1,17 @@
 package edu.badpals.repositorio;
 
+import java.util.Optional;
+
+import edu.badpals.domain.Wizard;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class Repositorio {
     @Inject
     RepoWizard repoWizard;
+    public Optional<Wizard> loadWizard(String name){
+       return this.repoWizard.findByIdOptional(name);
+
+    }
 }
